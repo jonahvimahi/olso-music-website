feb5349fb33da5c55f947429930904d9be70d205
jonahvimahi <jonahvimahi@gmail.com>
2021-10-05 16:24:40 -0600

    added media queries

--- app1/src/Components/Body.js
+++ app1/src/Components/Body.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import './Body.css'
+import React from "react";
+import "./Body.css";
 const Body = () => {
-    return(
-        <div id="body">
-        <div id="spotifyEmbedding">
-            <iframe id="comeHomeEmbedding"title="Come Home"src="https://open.spotify.com/embed/track/0tAAlZJ39X9SG5Tw8ubLgP?theme=0" allowTransparency="true" height="350vh" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        </div>
-        </div>
-    )
-}
-export default Body
+	return (
+		<div id="body">
+			<div id="spotifyEmbedding">
+				<div id="comeHomeColdDiv">
+					<iframe
+						id="comeHomeEmbedding"
+						title="Come Home"
+						src="https://open.spotify.com/embed/track/0tAAlZJ39X9SG5Tw8ubLgP?theme=1"
+						allowTransparency="true"
+						height="350vh"
+						frameBorder="0"
+						allowfullscreen=""
+						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					></iframe>
+					<iframe
+						id="coldEmbedding"
+						title="Cold"
+						src="https://open.spotify.com/embed/track/1kni4OdbmFPZsH3hxWGCXS?theme=1"
+						height="350vh"
+						frameBorder="0"
+						allowfullscreen=""
+						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					></iframe>
+				</div>
+				<div id="foolDiv">
+					<iframe
+						id="foolEmbedding"
+						title="Fool"
+						src="https://open.spotify.com/embed/track/61pMmoHEwW3PnghmZW1IMm"
+						height="80"
+						frameBorder="0"
+						allowfullscreen=""
+						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					></iframe>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Body;
