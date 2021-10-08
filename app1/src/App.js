42d2461c3cc79c99d3bc4ebba097a48cee4f7adb
jonahvimahi <jonahvimahi@gmail.com>
2021-10-08 11:29:18 -0600

    cleaning up code

--- app1/src/App.js
+++ app1/src/App.js
@@ -4,14 +4,6 @@ import "./Reset.css";
 import Home from "./Components/Home/Home";
 
 export default class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state = { matches: window.matchMedia("(min-width: 600px)").matches };
-	}
-	componentDidMount() {
-		const handler = e => this.setState({ matches: e.matches });
-		window.matchMedia("(min-width: 600px)").addListener(handler)
-	}
 	render() {
 		return (
 			<div className="backGround ">
